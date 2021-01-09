46668f7c7e7a0a3ebee4b55f1995d63a5fba4f07
stevenluissl <grandtemperal@gmail.com>
2021-01-09 12:49:18 +0700

    PKTI 2

--- app/src/main/java/com/example/foodtower/homepage.kt
+++ app/src/main/java/com/example/foodtower/homepage.kt
@@ -2,10 +2,30 @@ package com.example.foodtower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import com.synnapps.carouselview.ImageListener
+import kotlinx.android.synthetic.main.activity_homepage.*
 
 class homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+        //image slider
+        val sampleImages = intArrayOf(
+            R.drawable.reccomendation,
+            R.drawable.gmbr1,
+            R.drawable.gmbr2,
+            R.drawable.gmbr3
+        )
+
+        val imagesListener = ImageListener { position: Int, imageView: ImageView? ->
+            if (imageView != null) {
+                imageView.setImageResource(sampleImages[position])
+            }
+        }
+//image slider
+            caroselView.pageCount = sampleImages.size
+            caroselView.setImageListener(imagesListener)
+//image slider
     }
 }
