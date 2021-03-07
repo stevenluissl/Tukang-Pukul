d51a3be439d12c6fe829903a6aba1d08f9808c5d
WillyLayanto <layanto86@gmail.com>
2021-03-07 18:46:02 +0700

    onsaveinstance

--- app/src/main/java/com/example/foodtower/adding_food.kt
+++ app/src/main/java/com/example/foodtower/adding_food.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_adding_food.*
 
+private const val EXTRA_NAMA = "EXTRA_NAMA"
 class adding_food : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +22,20 @@ class adding_food : AppCompatActivity() {
             val homepage = Intent(this@adding_food,homepage::class.java)
             startActivity(homepage)
         }
+
+        button3.setOnClickListener {
+            editTextTextPersonName10.text = editTextTextPostalAddress.text
+            editTextTextPostalAddress.setText("")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(EXTRA_NAMA, editTextTextPersonName10.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        editTextTextPersonName10.text = savedInstanceState?.getString(EXTRA_NAMA) ?: "Kosong"
     }
 }
