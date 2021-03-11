e92ead206dbef2e3eced799448e1e4745a810791
Erwinn21 <erwiinn21@gmail.com>
2021-03-11 22:33:14 +0700

    button

--- app/src/main/java/com/example/foodtower/MainActivity3.kt
+++ app/src/main/java/com/example/foodtower/MainActivity3.kt
@@ -1,9 +1,7 @@
 package com.example.foodtower
 
-import android.app.Person
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.android.synthetic.main.activity_main3.*
 
 class MainActivity3 : AppCompatActivity() {
