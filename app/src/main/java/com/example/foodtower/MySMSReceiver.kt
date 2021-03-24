61b1856ad59dce11b1a0e5cf6fd72d0032132dd2
Erwinn21 <erwiinn21@gmail.com>
2021-03-24 20:56:55 +0700

    Merge branch 'master' of https://github.com/stevenluissl/Tukang-Pukul
     Conflicts:
    	app/src/main/java/com/example/foodtower/adding_food.kt

--- app/src/main/java/com/example/foodtower/MySMSReceiver.kt
+++ app/src/main/java/com/example/foodtower/MySMSReceiver.kt
@@ -1,14 +1,18 @@
 package com.example.foodtower
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.provider.Telephony
 import android.telephony.SmsMessage
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MySMSReceiver : BroadcastReceiver() {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceive(context: Context, intent: Intent) {
         if(intent.action.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)){
             var pdu = (intent.extras!!.get("pdus") as Array<*>).get(0)
