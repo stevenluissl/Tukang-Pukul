6c03336a5a8dd43efcd32f68dc06d586f8f63aca
WillyLayanto <layanto86@gmail.com>
2021-03-24 20:50:33 +0700

    sms receiver

--- app/src/main/java/com/example/foodtower/MySMSReceiver.kt
+++ app/src/main/java/com/example/foodtower/MySMSReceiver.kt
@@ -3,17 +3,20 @@ package com.example.foodtower
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.provider.Telephony
 import android.telephony.SmsMessage
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MySMSReceiver : BroadcastReceiver() {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceive(context: Context, intent: Intent) {
         if(intent.action.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)){
             var pdu = (intent.extras!!.get("pdus") as Array<*>).get(0)
             var myBundle = intent.extras
-            var format = myBundle?.getString("format")
+            var format = myBundle!!.getString("format")
             pdu.let{
                 var message = SmsMessage.createFromPdu(it as ByteArray,format)
                 var pesan = message.displayMessageBody
