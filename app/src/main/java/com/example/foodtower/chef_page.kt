b858f771ae12818d38ab3843dee56017da3755f5
WillyLayanto <layanto86@gmail.com>
2021-04-07 22:16:36 +0700

    jobscheduler

--- app/src/main/java/com/example/foodtower/chef_page.kt
+++ app/src/main/java/com/example/foodtower/chef_page.kt
@@ -1,16 +1,22 @@
 package com.example.foodtower
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_chef_page.*
 import kotlinx.android.synthetic.main.activity_food_menu1.*
 import kotlinx.android.synthetic.main.activity_homepage.*
 
 
 class chef_page : AppCompatActivity() {
+    var JobSchedulerID = 37
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chef_page)
@@ -42,5 +48,21 @@ class chef_page : AppCompatActivity() {
             intentSMS.putExtra("sms_body", "")
             startActivity(intentSMS)
         }
+
+        foodrec.setOnClickListener {
+            startJob()
+        }
+    }
+
+    private fun startJob() {
+        var serviceComponent = ComponentName(this,FoodRec::class.java)
+        var myJobInfo = JobInfo.Builder(JobSchedulerID, serviceComponent)
+            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+            .setRequiresDeviceIdle(false)
+            .setRequiresCharging(false)
+            .setPeriodic(3*60*1000)
+        var JobFood = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        JobFood.schedule(myJobInfo.build())
+        Toast.makeText(this,"Job Service Berjalan", Toast.LENGTH_SHORT).show()
     }
 }
