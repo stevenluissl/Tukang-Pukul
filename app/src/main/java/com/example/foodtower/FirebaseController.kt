f0974e1185e28916d3abece76521d58008cca176
Erwinn21 <erwiinn21@gmail.com>
2021-06-28 22:51:55 +0700

    a

--- app/src/main/java/com/example/foodtower/FirebaseController.kt
+++ app/src/main/java/com/example/foodtower/FirebaseController.kt
@@ -9,28 +9,41 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class FirebaseController (context: Context) {
+    //implementasi database firebase
     private var database = Firebase.database
+    //mendapatkan data referensi dari path USERS pada database firebase
     private val reference = database.getReference("USERS")
+    //implementasi context
     private val myContext = context
 
+    //function untuk menympan data pada database
     fun saveUser(users: DataClassFirebase) {
+        //deklarasi userID untuk mengirimkan data pada database USERS
         var usersID = reference.push().key.toString()
 
+        //untuk menambahkan cabang child pada database USERS
         reference.child(usersID).setValue(users).apply {
+            //jika berhasil maka akan menampilkan toast Data Saved
             addOnCompleteListener {
                 Toast.makeText(myContext,"Data Saved.",Toast.LENGTH_SHORT)
             }
             addOnCanceledListener {  }
+            //jika gagal amaka akan menampilkan toast Save Data Failed
             addOnFailureListener {
                 Toast.makeText(myContext,"Save Data Failed.",Toast.LENGTH_SHORT)
             }
             addOnSuccessListener {  }
         }
     }
+
+    //function untuk men-detect data nama pada database
     private fun readUsername() : MutableMap<String,String> {
+        //implementasi hasil yang merupakan muttable map
         var hasil = mutableMapOf<String,String>()
+        //mencari data pada database
         reference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
+                //jika database terdapat data
                 if(snapshot!!.exists()) {
                     hasil.clear()
                     for (data in snapshot.children) {
@@ -49,6 +62,7 @@ class FirebaseController (context: Context) {
         })
         return hasil
     }
+
     fun deleteUser(srcUsername : String) {
         val key = readUsername().filterValues { it == srcUsername }.keys
         reference.child(key.first())
